b5d3653b571e1f8aeb445435753e0af04869485b
Liam <51160700+sha0001@users.noreply.github.com>
2022-03-10 16:56:06 +1100

    Add files via upload
    Uploaded from initial build on CodePen

---
+++ simple-html-calculation-form/README.markdown
@@ -0,0 +1,5 @@
+# Simple HTML calculation form
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/sha0001/pen/xxLJjLo](https://codepen.io/sha0001/pen/xxLJjLo).
+
+
---
+++ simple-html-calculation-form/src/script.js
@@ -0,0 +1,25 @@
+function calculateTotal()
+{  let calculation={}
+  
+  calculation.LPP = Math.round(($("#volume").val() * $("#UPP").val()/1000 ))
+  $("#LPP").val(calculation.LPP);  
+  
+  calculation.AnnPall = Math.round(($("#AnnualUnits").val() / $("#UPP").val() ))
+  $("#AnnPall").val(calculation.AnnPall); 
+  
+  calculation.AnnShipCost = Math.round(($("#AnnPall").val() * $("#SPP").val()))
+  $("#AnnShipCost").val(calculation.AnnShipCost); 
+  
+  calculation.POH = Math.round(($("#SOH").val() / $("#UPP").val()))
+  $("#POH").val(calculation.POH); 
+  
+  calculation.SCequiv = Math.round(($("#POH").val() / $("#SCPallets").val()))
+  $("#SCequiv").val(calculation.SCequiv); 
+  
+}
+
+$(function()
+ {
+    $(".qty").on("change keyup",calculateTotal)
+})
+
